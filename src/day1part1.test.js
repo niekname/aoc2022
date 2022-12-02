8dbc59583f98b7347bd01103a1f55384352c0a35
Niek Vervaeke <niek.vervaeke@gmail.com>
2022-12-02 22:53:45 +0100

    day 1 part 1

--- src/day1part1.test.js
+++ src/day1part1.test.js
@@ -1,4 +1,34 @@
-test("it works", () => {
-  const itWorks = true;
-  expect(itWorks).toBe(true);
+const { sum, first } = require("lodash");
+
+function mostCaloriesCarriedByElf(input) {
+  return first(
+    input
+      .split("\n\n")
+      .map((cpe) => sumOfGroup(cpe))
+      .sort((a, b) => b - a)
+  );
+
+  function sumOfGroup(cpe) {
+    return sum(cpe.split("\n").map((c) => +c));
+  }
+}
+
+test("most calories carried by elf", () => {
+  const input =
+    "1000\n" +
+    "2000\n" +
+    "3000\n" +
+    "\n" +
+    "4000\n" +
+    "\n" +
+    "5000\n" +
+    "6000\n" +
+    "\n" +
+    "7000\n" +
+    "8000\n" +
+    "9000\n" +
+    "\n" +
+    "10000";
+  const result = mostCaloriesCarriedByElf(input);
+  expect(result).toBe(24000);
 });
